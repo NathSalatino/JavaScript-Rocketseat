626a2f85cf7b59be4facbf4c01de98023b0933ad
Nathalia Salatino <113551133+NathSalatino@users.noreply.github.com>
2022-10-24 17:27:03 -0300

    exercicios1

--- script.js
+++ script.js
@@ -1,5 +1,38 @@
-// Assim eu faço um comentário em linha.
+// let weight;
+// console.log(typeof weight)
 
-/* 
-Aqui eu tenho um comentário multilinhas.
-*/
+let name = "Mayk"
+let age = 23
+let weight = 80.8
+let isSubscribed = true
+
+let student = {
+    name: "Nathalia",
+    age: 26,
+    weight: 70.6,
+    isSubscribed: true
+}
+
+console.log(`${student.name} de idade ${student.age} pesa ${student.weight} kg.`)
+
+let students = []
+
+students = [
+    student
+]
+
+console.log(students)
+console.log(students[0])
+
+const john = {
+    name: "John",
+    age: 50,
+    weight: 55.9,
+    isSubscribed: true
+}
+
+students = [
+    student,
+    john
+]
+console.log(students)
